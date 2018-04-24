2ea38a1bb4b50dee4ecbfa463754502deb8df83f
Leejames Merlas <beeboks34@gmail.com>
2018-04-24 10:49:14 +0800

    some

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from prisonapp import app
+import os
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    #app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
