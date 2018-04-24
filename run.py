6dd596013b77e4befb8b2a99fb216828930bc6fb
Leejames Merlas <beeboks34@gmail.com>
2018-04-24 14:15:49 +0800

    lmaopleasework

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ from prisonapp import app
 import os
 
 
+port = os.getenv('PORT', '5000')
+
 if __name__ == '__main__':
     #app.run()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(port))
 
